d10b8b8bc31434c1be4f4aac09bf8e1ce914652d
Alextufeng <salman-aka@mail.ru>
2022-05-02 17:26:28 +0300

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== "string") return false;
+  
+  sampleActivity = Number(sampleActivity);
+  
+  if (sampleActivity === '0' || typeof sampleActivity !== 'string' && (sampleActivity > 0 && sampleActivity <= MODERN_ACTIVITY)) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) * (HALF_LIFE_PERIOD / Math.log(2)));
+  } else {
+    return false
+  }
 }
 
 module.exports = {
